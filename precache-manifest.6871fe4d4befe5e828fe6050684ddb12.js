de835ac3af133bcd773df5b52dafd26ee7ff4fa2
tuanconbu <hieutk5@gmail.com>
2020-08-03 16:10:23 +0700

    Updates

---
+++ precache-manifest.6871fe4d4befe5e828fe6050684ddb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08923b9cb8c926686a0a6d47f9b89392",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "ed3bdfb9c1eca432a2e4",
+    "url": "/quiz-app/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "cb17fc3a747bb2715c2f",
+    "url": "/quiz-app/static/css/main.ff2cfd0b.chunk.css"
+  },
+  {
+    "revision": "ed3bdfb9c1eca432a2e4",
+    "url": "/quiz-app/static/js/2.fdd2fde0.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/quiz-app/static/js/2.fdd2fde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb17fc3a747bb2715c2f",
+    "url": "/quiz-app/static/js/main.aae453e0.chunk.js"
+  },
+  {
+    "revision": "d72113544e3feea9fe6f",
+    "url": "/quiz-app/static/js/runtime-main.c56677b8.js"
+  }
+]);
